ffde7b639d7a6203f6ffd86c9d71548823af2ee4
Rafael Kazuo Sato Simião <rafael.kazuo@outlook.com>
2020-05-16 16:30:16 -0300

    aspbotDebug, aspbotRuntime, debugAdapter, extension

---
+++ src/debugAdapter.ts
@@ -0,0 +1,7 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+
+ import { AspBotDebugSession } from './aspbotDebug';
+
+ AspBotDebugSession.run(AspBotDebugSession);
